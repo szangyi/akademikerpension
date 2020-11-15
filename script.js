025b459c14201a9e26395c52988f013ce7a6936a
elen1935 <elen1935@stud.kea.dk>
2020-11-15 23:31:01 +0100

    allow dom fully load, edit object naming

--- script.js
+++ script.js
@@ -1,65 +1,35 @@
-//function init() {
-//    fetch("http://a-day.dk/T9/wp-json/wp/v2/posts").then(r => r.json()).then(function (data) {
-//        categoriesReceived(data)
-//    })
-//}
-//
-//init();
+window.addEventListener('DOMContentLoaded', getData);
 
+const datalink = "http://efcreations.es/t9w1/wp-json/wp/v2/change?_embed";
 
-
-
-
-/*fetch("http://www.a-day.dk/module-09/recreate/wp-json/wp/v2/property?_embed").then(r => r.json()).then(handleData)
-
-
-function handleData(posts) {
-    //    console.log(posts);
-    posts.forEach(showPost)
+function getData(){
+    fetch(datalink)
+        .then(res => res.json())
+        .then(handleData);
 }
 
-function showPost(post) {
-    console.log(post);
-    const template = document.querySelector("template").content;
 
-    const copy = template.cloneNode(true);
-//    const img = copy.querySelector(".postimage");
-//
-//    img.setAttribute("src", `https://www.a-day.dk/module-09/recreate/wp-json/wp/v2/property?_embedded["wp:featuredmedia"][0].link`);
-
-    copy.querySelector(".postimage").outerHTML = post._embedded["wp:featuredmedia"][0].link;
-    //could not fix image :(
-
-
-    copy.querySelector("h2").textContent = post.title.rendered;
-    copy.querySelector(".bed").textContent = post.bed;
-    copy.querySelector(".bath").textContent = post.bath;
-    copy.querySelector(".sqfeet").textContent = post.sq_feet;
-
-    document.querySelector("main").appendChild(copy);
-}*/
-
-fetch("http://efcreations.es/t9w1/wp-json/wp/v2/change")
+/*fetch("http://efcreations.es/t9w1/wp-json/wp/v2/change")
     .then(initial => initial.json())
     .then(handleData);
+*/
 
 function handleData(data) {
-    console.log(data)
-    data.forEach(showPost)
+    console.log(data);
+    data.forEach(showChange);
 }
 
-function showPost(post) {
-    console.log(post)
+function showChange(change) {
+    console.log(change)
     const template = document.querySelector("template#productTemplate").content;
     const copy = template.cloneNode(true);
+    //console.log(change._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
 
-    copy.querySelector(".data_title").textContent = post.title.rendered;
-    copy.querySelector(".data_shortdescription").textContent = post.short_description;
-    //copy.querySelector(".product_image").src = post._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    copy.querySelector(".data_title").textContent = change.title.rendered;
+    copy.querySelector(".data_shortdescription").textContent = change.short_description;
+    copy.querySelector(".product_image").src = change._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
 
-
-
-    /*copy.querySelector(".content").innerHTML = post.content.rendered;*/
+    /*copy.querySelector(".content").innerHTML = change.content.rendered;*/
 
     document.querySelector("main").appendChild(copy);
 }
