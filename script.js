82ed517f4a656569e4fa79e59a803a7510f47f6a
elen1935 <elen1935@stud.kea.dk>
2020-11-17 12:45:28 +0100

    another testing categories

--- script.js
+++ script.js
@@ -26,37 +26,46 @@ function getData() {
 }
 
 function getNav() {
-  fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories?parent=18&orderby=count&order=desc")
-    .then(res => res.json())
-    .then(handleCategoryNavData)
+    fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories?parent=18&orderby=count&order=desc")
+        .then(res => res.json())
+        .then(handleCategoryNavData)
 }
 
 function handleCategoryNavData(categories) {
-  categories.forEach(addNavLink);
+    categories.forEach(addNavLink);
 }
 
 function addNavLink(oneCategory) {
-  //console.log("cat");
-  //console.log(oneCategory);
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-  a.href = "category.html?cat_id=" + oneCategory.id;
-  a.textContent = oneCategory.name;
-  console.log(a);
-  li.appendChild(a);
-  document.querySelector(".filternav").appendChild(li);
+    console.log("cat");
+    //console.log(oneCategory);
+    const a = document.createElement('a');
+    a.textContent = oneCategory.name;
+    a.href = "category.html?cat_id=" + oneCategory.id;
+    console.log(a);
+    document.querySelector('section').appendChild(a);
 }
 
+/*function addNavLink(oneCategory) {
+    console.log("cat");
+    //console.log(oneCategory);
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = oneCategory.name;
+    a.href = "category.html?cat_id=" + oneCategory.id;
+    console.log(a);
+    li.appendChild(a);
+    document.querySelector(".filternav").appendChild(li);
+}*/
 
 
 function handleData(data) {
-    console.log(data);
+    //console.log(data);
     data.forEach(showChange);
 }
 
 function showChange(change) {
     //console.log(change)
-    console.log(change.short_description)
+    //console.log(change.short_description)
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     console.log(change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url)
