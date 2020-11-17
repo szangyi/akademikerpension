914591ecfb7fa9f6ce2b7a62dd5126e6313876e8
elen1935 <elen1935@stud.kea.dk>
2020-11-17 14:49:34 +0100

    separate section on template, create elements html

--- script.js
+++ script.js
@@ -28,25 +28,50 @@ function getData() {
 function getNav() {
     fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories?parent=18&orderby=count&order=desc")
         .then(res => res.json())
-        .then(handleCategoryNavData)
+        .then(data => handleCategoryNavData(data))
+
 }
 
 function handleCategoryNavData(categories) {
+    console.log("in handleCategory", categories)
     categories.forEach(addNavLink);
 }
 
 function addNavLink(oneCategory) {
-    console.log("cat");
+    //create another js just for categories and fetch from the link below
+
+    /*console.log("cat", oneCategory);
+    fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories/"+oneCategory.id)
+        .then(res => res.json())
+        .then((data)=>let categoryData=data)*/
     //console.log(oneCategory);
+
+    const template = document.querySelector("#filterTemplate").content;
+    const copy = template.cloneNode(true);
+    console.log(copy)
+    copy.querySelector('a').textContent = oneCategory.name;
+    copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
+    document.querySelector("#filtercontainer").appendChild(copy);
+
+    /*
+        const template = document.querySelector("#filterTemplate").content;
+    const copy = template.cloneNode(true);
+
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.textContent = oneCategory.name;
-    a.href = "category.html?cat_id=" + oneCategory.id;
+    copy.querySelector('a').textContent = oneCategory.name;
+    console.log(oneCategory.name)
+    copy.a.href = "category.html?cat_id=" + oneCategory.id;
     console.log(a);
     li.appendChild(a);
-    document.querySelector(".filternav").appendChild(li);
-    document.querySelector('section').appendChild(a);
+    console.log(document.querySelector('section'))
+//    document.querySelector(".filterul").appendChild(li);
+    template.appendChild(li);
+    */
 }
+
+
+//http://lasseclaes.com/20f/2nd_sem_int/wp/wp-json/wp/v2/bikes/?categories=7&_embed
 
 
 function handleData(data) {
@@ -57,7 +82,7 @@ function handleData(data) {
 function showChange(change) {
     //console.log(change)
     //console.log(change.short_description)
-    const template = document.querySelector("template").content;
+    const template = document.querySelector("#changeTemplate").content;
     const copy = template.cloneNode(true);
     console.log(change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url)
 
@@ -65,7 +90,7 @@ function showChange(change) {
     copy.querySelector(".shortdescription").textContent = change.short_description;
     copy.querySelector(".product_image").src = change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
     //copy.querySelector('article').style.backgroundImage = "url('https://it-studerende.dk/test/student.png')";
-//    copy.querySelector('article').style.backgroundImage = change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+    //copy.querySelector('article').style.backgroundImage = change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
 
     /*copy.querySelector(".content").innerHTML = change.content.rendered;*/
 
