e58d2ea1c5714bd467c0c939e491d36dcd814617
Alexandra Mészáros <alexandrameszaros@Alexandras-MacBook-Pro-2.local>
2020-11-18 16:43:36 +0100

    navigation fixed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', getData);
 const datalink = "http://efcreations.es/t9w1/wp-json/wp/v2/change?_embed";
 
 function getData() {
-    getNav()
+//    getNav()
     const urlParams = new URLSearchParams(window.location.search);
     console.log("URLSearchParams " + window.location);
     const the_change_id = urlParams.get("change_id"); //getting the id from the URL
@@ -23,32 +23,32 @@ function getData() {
     }
 }
 
-function getNav() {
-    fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories?parent=18&orderby=count&order=desc")
-        .then(res => res.json())
-        .then(data => handleCategoryNavData(data))
+//function getNav() {
+//    fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories?parent=18&orderby=count&order=desc")
+//        .then(res => res.json())
+//        .then(data => handleCategoryNavData(data))
+//
+//}
+//
+//function handleCategoryNavData(categories) {
+//    console.log("in handleCategory", categories)
+//    categories.forEach(addNavLink);
+//}
 
-}
-
-function handleCategoryNavData(categories) {
-    console.log("in handleCategory", categories)
-    categories.forEach(addNavLink);
-}
-
-function addNavLink(oneCategory) {
-    /*console.log("cat", oneCategory);
-    fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories/"+oneCategory.id)
-        .then(res => res.json())
-        .then((data)=>let categoryData=data)*/
-    //console.log(oneCategory);
-
-    const template = document.querySelector("#filterTemplate").content;
-    const copy = template.cloneNode(true);
-    console.log(copy)
-    copy.querySelector('a').textContent = oneCategory.name;
-    copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
-    document.querySelector("#filtercontainer").appendChild(copy);
-}
+//function addNavLink(oneCategory) {
+//    /*console.log("cat", oneCategory);
+//    fetch("http://efcreations.es/t9w1/wp-json/wp/v2/categories/"+oneCategory.id)
+//        .then(res => res.json())
+//        .then((data)=>let categoryData=data)*/
+//    //console.log(oneCategory);
+//
+//    const template = document.querySelector("#filterTemplate").content;
+//    const copy = template.cloneNode(true);
+//    console.log(copy)
+//    copy.querySelector('a').textContent = oneCategory.name;
+//    copy.querySelector('a').href = "category.html?cat_id=" + oneCategory.id;
+//    document.querySelector("#filtercontainer").appendChild(copy);
+//}
 
 
 /*
@@ -76,18 +76,18 @@ function handleData(data) {
     data.forEach(showChange);
 }
 
+
 function showChange(change) {
-    //console.log(change)
-    //console.log(change.short_description)
-    const template = document.querySelector("#changeTemplate").content;
+//    console.log(change)
+    console.log(change.short_description)
+    const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
-    //console.log(change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url)
+    console.log(change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url)
 
     copy.querySelector(".title").textContent = change.title.rendered;
     copy.querySelector(".title2").textContent = change.title.rendered;
     copy.querySelector(".shortdescription").textContent = change.short_description;
     copy.querySelector(".product_image").src = change._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
-
 
     const a = copy.querySelector('a');
     if (a) {
